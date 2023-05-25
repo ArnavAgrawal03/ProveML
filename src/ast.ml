4f7eaca7a4f0de78f31e0c689a687ddcc4e68b38
Arnav Agrawal <aa779@cornell.edu>
2023-05-25 12:31:39 +0530

    create proper AST and cnf

--- src/ast.ml
+++ src/ast.ml
@@ -1,15 +1,32 @@
 (* Start AST *)
 
-type bop =
-  | Or
-  | Add
-  | Impl
-  | Equiv
-
-type uop = Not
-type value = bool
-
 type prop =
-  | Prop of value
-  | Binop of bop * prop * prop
-  | Unop of uop * prop
+  | Atom of string
+  | Or of prop * prop
+  | And of prop * prop
+  | Not of prop
+  | Imp of prop * prop
+  | Iff of prop * prop
+  | True
+  | False
+
+type literal =
+  | Pos of string
+  | Neg of string
+
+type clause = literal list
+type cnf = clause list
+
+let rec convert_to_base (p : prop) =
+  match p with
+  | Atom _ | True | False -> p
+  | Or (a, b) -> Or (convert_to_base a, convert_to_base b)
+  | And (a, b) -> And (convert_to_base a, convert_to_base b)
+  | Not a -> Not (convert_to_base a)
+  | Imp (a, b) -> Or (Not (convert_to_base a), convert_to_base b)
+  | Iff (a, b) -> convert_to_base (And (Imp (a, b), Imp (b, a)))
+
+let rec convert_to_cnf (p : prop) =
+  match p with
+  | Atom a -> [ [ Pos a ] ]
+  | _ -> failwith "Not implemented"
