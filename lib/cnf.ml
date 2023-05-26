dbfe6ffb398aeeb379f32e6b792c077b408bda36
Arnav Agrawal <aa779@cornell.edu>
2023-05-26 22:06:03 +0530

    Add string_of_cnf functionality, specification for interface

--- lib/cnf.ml
+++ lib/cnf.ml
@@ -1,6 +1,6 @@
 open Prop
 
-(* start cnf*)
+(*helper/shielded functions specified here. All others sepcified in the interface file*)
 
 type literal =
   | Pos of string
@@ -34,12 +34,15 @@ let rec nnf_of_prop (p : prop) : nnf =
   | Imp (a, b) -> nnf_of_prop (Or (Not a, b))
   | Iff (a, b) -> nnf_of_prop (And (Imp (a, b), Imp (b, a)))
 
+(** [distribute a b] distributes the [Or] operator to both literals of an and operator*)
 let rec distribute (a : nnf) (b : nnf) : nnf =
   match (a, b) with
   | N_And (a1, a2), b -> N_And (distribute a1 b, distribute a2 b)
   | a, N_And (b1, b2) -> N_And (distribute a b1, distribute a b2)
   | a, b -> N_Or (a, b)
 
+(** Processes nnf such that [And] constructors are completely on the outside. Resulting
+    nnf matches the precondition for [cnf_of_distr]*)
 let rec distributed_nnf (n : nnf) : nnf =
   match n with
   | N_True -> N_True
@@ -48,20 +51,28 @@ let rec distributed_nnf (n : nnf) : nnf =
   | N_Or (a, b) -> distribute (distributed_nnf a) (distributed_nnf b)
   | N_And (a, b) -> N_And (distributed_nnf a, distributed_nnf b)
 
-let rec list_of_ors (n : nnf) =
-  match n with N_And (a, b) -> list_of_ors a @ list_of_ors b | _ -> [ n ]
+(** Given a set of nested ands, [n], [list_of_ands n] flattens [n] to a list of nnfs as a
+    disjunction. Helper function to convert from nnf to cnf. Requires: [n] is already
+    distributed in terms of [Or]. The resulting value does not contain any [And]
+    constructors*)
+let rec list_of_ands (n : nnf) =
+  match n with N_And (a, b) -> list_of_ands a @ list_of_ands b | _ -> [ n ]
 
+(** Given a set of nested ors, [n], [clause_of_ors n] flatten [n] to a list of literals as
+    a conjunction. Requires: [n] doesn't contain any ands. *)
 let rec clause_of_ors (n : nnf) =
   match n with
   | Lit a -> [ a ]
   | N_Or (a, b) -> clause_of_ors a @ clause_of_ors b
   | _ -> []
 
+(** [cnf_of_distr n] is the cnf version of [n]. Requires: nnf already has distrubuted
+    form.*)
 let cnf_of_distr (n : nnf) : cnf =
   match n with
   | N_True -> []
   | N_False -> [ [] ]
-  | _ -> list_of_ors n |> List.map clause_of_ors
+  | _ -> list_of_ands n |> List.map clause_of_ors
 
 let cnf_of_prop p = p |> nnf_of_prop |> distributed_nnf |> cnf_of_distr
 
@@ -73,3 +84,11 @@ let rec string_of_nnf (n : nnf) : string =
   | Lit (Neg a) -> "~" ^ a
   | N_And (a, b) -> "(" ^ string_of_nnf a ^ " ^ " ^ string_of_nnf b ^ ")"
   | N_Or (a, b) -> "(" ^ string_of_nnf a ^ " V " ^ string_of_nnf b ^ ")"
+
+let string_of_literal l = match l with Pos a -> a | Neg a -> "~" ^ a
+
+let string_of_list_aux converter joiner lst =
+  "(" ^ (lst |> List.map converter |> List.fold_left (fun a b -> a ^ joiner ^ b) "") ^ ")"
+
+let string_of_clause = string_of_list_aux string_of_literal " V "
+let string_of_cnf = string_of_list_aux string_of_clause " ^ "
--- lib/cnf.mli
+++ lib/cnf.mli
@@ -1,9 +1,11 @@
 open Prop
 
+(** A literal in propositional logic*)
 type literal =
   | Pos of string
   | Neg of string
 
+(** Represents the Negation Normal form of a statement in propositional logic*)
 type nnf =
   | N_True
   | N_False
@@ -12,13 +14,20 @@ type nnf =
   | N_Or of nnf * nnf
 
 type clause = literal list
+(** A clause disjunction of literal*)
+
 type cnf = clause list
+(** The Conjunctive Normal form of a statement in propositional logic. A conjunction of
+    clauses.*)
 
 val nnf_of_prop : prop -> nnf
-val distribute : nnf -> nnf -> nnf
-val distributed_nnf : nnf -> nnf
-val list_of_ors : nnf -> nnf list
-val clause_of_ors : nnf -> literal list
-val cnf_of_distr : nnf -> cnf
+(** [nnf_of_prop p] is the Negation Normal form of [p]*)
+
 val cnf_of_prop : prop -> cnf
+(** [cnf_of_prop p] is the Conjunctive Normal Form of [p]*)
+
 val string_of_nnf : nnf -> string
+(** [string_of_nnf n] is the string representing [n]*)
+
+val string_of_cnf : cnf -> string
+(** [string_of_cnf c] is the string representing [c]*)
